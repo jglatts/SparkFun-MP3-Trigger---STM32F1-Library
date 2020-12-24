1127bb5093071196397990e054bfee95f0fb48e4
John Glatts <john@z-axiscc.com>
2020-12-24 15:06:07 -0500

    Update mp3_trigger.h

--- mp3_trigger.h
+++ mp3_trigger.h
@@ -11,13 +11,20 @@
 
 #include "stm32f1xx_hal.h"
 
+typedef enum {
+	MP3_ERROR,
+	MP3_OK
+} MP3_STATE;
+
 #define NO_DELAY 0
 #define STD_DELAY 5
 
 uint8_t trig_status_buff[30];
 
-void MP3_get_trigger_status(UART_HandleTypeDef *huart1);
-void MP3_wait_for_song_finish(UART_HandleTypeDef *huart1);
-void MP3_run_trig_cmd(UART_HandleTypeDef *huart1, uint8_t, uint8_t, uint16_t);
+MP3_STATE MP3_UART_Init(UART_HandleTypeDef*);
+void MP3_Get_Trigger_Status(UART_HandleTypeDef *huart1);
+void MP3_Wait_For_Song_Finish(UART_HandleTypeDef *huart1);
+void MP3_Run_Trig_Cmd(UART_HandleTypeDef *huart1, uint8_t, uint8_t, uint16_t);
+void MP3_Play_Song(UART_HandleTypeDef *huart1, uint8_t num);
 
 #endif /* INC_MP3_TRIGGER_H_ */
